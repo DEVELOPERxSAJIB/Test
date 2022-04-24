200fc12b0d6b39f0848f51da936904cb2d7a59d5
Md SaJib Shikder <98666041+DEVELOPERxSAJIB@users.noreply.github.com>
2022-04-24 12:57:45 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,250 @@
+// let name = null;
+
+// console.log(name ?? 'Namer value dewa nai'); //Null Colasing
+
+// let alu = '100.68';
+// let lao = '120.87';
+
+// let mot = Number(alu) + parseFloat(lao);
+
+// console.log(`${mot} taka`);
+
+// let age = ' ';
+
+// if(age){
+//     console.log(`True Value`);
+// }else{
+//     console.log(`False Value`);
+// }
+
+// let name ='Sajib';
+// let age = 6;
+
+// let rrr = age >= 4 && age <=10 ? 'Your now ready' : 'You are not allowed'
+
+// console.log(rrr);
+
+// let name = 'sajib';
+// let age = 23;               //Ternary Operator
+
+// let mrg = age >= 21 ? 'mrg is okey' : 'marrage is hobena tmr';
+// console.log(mrg);
+
+//Ternary Oprator
+// let name = 'Sajib';
+// let love = true;
+
+// let prog = love == true ? `ami ${name} k valobshi` : `he is really fcker`
+
+// console.log(prog);
+
+//Nulish colasing
+// let name = 'Brazil';
+
+// console.log(name ?? `Name not set`);
+
+// == vs ==
+// let num1 = '120';
+// let num2 = 120;
+
+// if(num1 === num2){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// let name = 'Dhaka';
+
+// let data = name ?? `Bangladesh`;
+
+// console.log(data);
+
+// let alu = prompt("Alu");
+// let potol = prompt("Potol");
+// let lau = prompt("Lau");
+// let kumra = prompt("Kumra");
+
+// let mot = +alu + +potol + +lau + +kumra;
+
+// console.log(`
+
+//     Alu                 = ${alu}
+//     Potol               = ${potol}
+//     Lau                 = ${lau}
+//     kumra               = ${kumra}
+// ----------------------------------------------
+//     Sob bazar           = ${mot} taka
+
+// `);
+
+
+
+// let currency = prompt('Enter the Currency');
+// let ammount = prompt('Enter the Ammount here');
+
+// if (currency == 'dollar'){
+//     rate = 85;
+// }else if (currency == 'euro'){
+//     rate = 98;
+// }else if (currency == 'pound'){
+//     rate = 114
+// }
+
+// let bdt = ammount * rate;
+
+// console.log(`
+
+//     ${ammount} ${currency} = ${bdt} Taka
+
+// `);
+
+
+
+// let name = prompt('What is you Name?');
+// let age = prompt('What is your Age');
+// let gender = prompt('What is your Gender');
+
+// let marry_age = 0;
+// let respect = '';
+
+// if(gender == 'Male'){
+//     marry_age = 21;
+//     respect = 'Vaiyaa';
+// }else if(gender == Female){
+//     marry_age = 18;
+//     respect = 'Apuu';
+// }
+
+
+// if( age >= marry_age){
+//     console.log(`Hi, ${name} ${respect}, Apnar biyer boyosh hoye gese`);
+// }else if (age < marry_age){
+//     console.log(`Sorry ${name} ${respect}, You are not able for marrage, Please wait`);
+// }
+
+
+
+// let currency = prompt('Currency');
+// let ammount = prompt('Ammount');
+
+// if (currency == 'dollar'){
+//     rate = 85;
+// }else if (currency == 'euro') {
+//     rate = 99;
+// }else if (currency == 'pound') {
+//     rate = 114;
+// }
+
+// let bdt = ammount * rate;
+
+// console.log(`
+
+//     ${ammount} ${currency} = ${bdt} taka
+
+// `);
+
+
+// let name = prompt('name');
+// let age = prompt('age');
+// let gneder = prompt('gender')
+
+
+// let biyer_boyosh = 0;
+// let somman = '';
+
+// if (gneder == 'male'){
+//     biyer_boyosh = 21;
+//     somman ='vaiyaa';
+// }else if(gneder == 'female'){
+//     biyer_boyosh =18;
+//     somman = 'apuu';
+// }
+
+
+// if(age >= biyer_boyosh){
+//     console.log(`Hi, ${name} ${somman}, apnar biyer boyosh hoye gese`);
+// } else if (age < biyer_boyosh){
+//     console.log(`Hi, ${somman}, anar ekhono biyer boyosh hoini`);
+// }
+
+
+// switch (gneder) {
+//     case 'male':
+//         biyer_boyosh = 21;
+//         somman = 'vaiyaa';
+//         break;
+
+//     case 'female':
+//         biyer_boyosh = 18;
+//         somman = 'apuuu';
+//         break;
+// }
+
+
+// if(age >= biyer_boyosh){
+//     console.log(`Hi, ${name} ${somman}, apnar biyer boyosh hoye gese`);
+// } else if (age < biyer_boyosh){
+//     console.log(`Hi, ${somman}, anar ekhono biyer boyosh hoini`);
+// }
+
+
+
+// let currency = prompt('currency');
+// let ammount = prompt('ammount');
+
+
+// let rate = 0;
+// switch (currency) {
+//     case 'dollar':
+//         rate = 85;
+//         break;
+
+//     case 'euro':
+//         rate = 99;
+//         break;
+
+//     case 'pound':
+//         rate = 114;
+//         break;
+// }
+
+// let bdt = ammount * rate;
+
+// console.log(`${ammount} ${currency} = ${bdt} taka`);
+
+
+// let name = null;
+
+// console.log(name ?? `Bangladesh`);
+
+
+
+// let name = 'sajib'
+// let golap = false;
+
+// let res = golap == false ? 'Mimma love sajib' : 'sajib mimmar dik a fir a takabe na'
+
+// console.log(`${res}`);
+
+
+let age = 3;
+switch(age){
+    case 1:
+        console.log('You are baby');
+        break;
+
+    case 5:
+        console.log('You are not ababy');
+        break;
+
+    case 10: 
+        console.log('Are you okey?');
+        break;
+
+    default:
+        console.log('All is okey');
+        break;
+}
+
+
+
